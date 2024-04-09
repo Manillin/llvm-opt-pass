7894ce43a77d2296d13e72ab52227756ddb4b2a0
Manillin <chri.vwm@gmail.com>
2024-04-09 15:59:09 +0200

    cleanup

--- LocalOpts.h
+++ LocalOpts.h
@@ -3,13 +3,13 @@
 
 #include "llvm/IR/PassManager.h"
 
-
-namespace llvm {
-    class LocalOpts : public PassInfoMixin<LocalOpts> {
-        public:
+namespace llvm
+{
+    class LocalOpts : public PassInfoMixin<LocalOpts>
+    {
+    public:
         PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
     };
 } // namespace llvm
-
 
 #endif // LLVM_TRANSFORMS_TESTPASS _H
