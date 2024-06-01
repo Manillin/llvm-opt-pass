6410aa02bcc220579c3f6e9a2ae6c93fa46728a1
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-06-01 18:14:50 +0200

    Init Assignment4

---
+++ Assignment4/source/Loop_fusion.c
@@ -0,0 +1,9 @@
+void twoloops(int a[] , int b[] , int c[] , int d[] , int n){
+    int i;
+
+    for(i=0 ; i<n ; i++)
+        a[i] = b[i] + c[i];
+    
+    for(i=0 ; i<n ; i++)
+        d[i] = a[i] * a[i];
+}
